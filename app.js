05a241deecf022eedc7e97406b4952861a1024f3
dikjain <dikshitmahanot2005@gmail.com>
2025-04-06 17:28:51 +0530

    error ki mkc

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.post('/join-meet', async (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
 });
